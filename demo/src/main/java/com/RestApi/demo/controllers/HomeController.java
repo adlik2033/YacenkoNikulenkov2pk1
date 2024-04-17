d4f4ed142b2ac54558ec40e6ed7cf3f64913b45b
2pk1_Jacenko <143966681+adlik2033@users.noreply.github.com>
2024-04-17 09:16:22 +0500

    Add files via upload

---
+++ demo/src/main/java/com/RestApi/demo/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.RestApi.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller@RequestMapping("/rest/home") public class HomeController {
+
+    @ResponseBody
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String hello(){
+        return "ответ с endpoint rest/home";
+    }
+}
